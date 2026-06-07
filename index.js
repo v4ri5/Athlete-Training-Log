4e795bcc77f155304248ca4530656be38d4dc84c
Joshua Sterling <Joshuasterling439@gmail.com>
2026-06-06 23:06:37 -0400

    more function for video

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TL_button = document.getElementById("TL_button");
 
 TL_button.addEventListener("click", ( function(){
-    window.open("./TrainingLog.html");
+    window.open("./AthleteForm.html");
 }))
 
 const VV_button = document.getElementById("VB");
