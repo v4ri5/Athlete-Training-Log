6cef4ca6871d29cac1cb4f96b960ff14fa9e042b
v4ri5 <tavarisct123@gmail.com>
2026-06-07 06:37:31 -0400

    Redesigned the landing page to match the Athlete form fixed the issues from the refactor, relinking the JS file to the landing page html and reconnecting the traininglog html and video vault html to the buttons.

--- JavaScript Code/index.js
+++ JavaScript Code/index.js
@@ -1,11 +1,11 @@
 const TL_button = document.getElementById("TL_button");
 
-TL_button.addEventListener("click", ( function(){
-    window.open("./AthleteForm.html");
-}))
+TL_button.addEventListener("click", function(){
+    window.open("/HTMLCode/TrainingLog.html");
+})
 
 const VV_button = document.getElementById("VB");
 
-VV_button.addEventListener("click", ( function(){
-    window.open("./VideoVault.html");
-}))
+VV_button.addEventListener("click", function(){
+    window.open("/HTMLCode/VideoVault.html");
+})
